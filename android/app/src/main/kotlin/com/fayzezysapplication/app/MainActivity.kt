3885e334054f46d49ee1ec5783955beab879dd99
fayzezy <fayz80361@gmail.com>
2024-01-10 08:16:55 +0000

    Code Synced by DhiWise

---
+++ android/app/src/main/kotlin/com/fayzezysapplication/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fayzezysapplication.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/presentation/product_detail_tab_container_screen/widgets/widget_item_widget.dart
@@ -0,0 +1,19 @@
+import 'package:fayzezy_s_application1/core/app_export.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class WidgetItemWidget extends StatelessWidget {
+  const WidgetItemWidget({Key? key})
+      : super(
+          key: key,
+        );
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomImageView(
+      imagePath: ImageConstant.imgProductImage238x375,
+      height: 238.v,
+      width: 375.h,
+    );
+  }
+}
